b4a41056cafa9f07d32a268ba356459ddd73866b
José Manuel Nieto <superjmn@outlook.com>
2024-05-20 12:34:05 +0200

    Castaña

--- HistoryPoC/ViewModels/MainViewModel.cs
+++ HistoryPoC/ViewModels/MainViewModel.cs
@@ -1,6 +1,85 @@
-﻿namespace HistoryPoC.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Reactive.Linq;
+using DynamicData;
+using DynamicData.Aggregation;
+
+namespace HistoryPoC.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+
+    public MainViewModel()
+    {
+        var items = new TransactionModel[]
+        {
+            new TransactionModel("Single", 1, null),
+            new TransactionModel("Single", 2, null),
+            new TransactionModel("Single", 3, null),
+            new TransactionModel("Two", 4, 1),
+            new TransactionModel("Three", 5, 1)
+        };
+
+        var sourceCache = new SourceCache<TransactionModel, int>(x => x.Id);
+        sourceCache.AddOrUpdate(items);
+
+        sourceCache.PopulateFrom(Observable.Interval(TimeSpan.FromSeconds(5)).Select(n => new[] { new TransactionModel("New", 6 + (int)n, 1) }));
+        
+
+        sourceCache
+            .Connect()
+            .Group(model => model.GroupId)
+            .Transform(g => (TransactionItem) new TransactionGroup(g))
+            .DisposeMany()
+            .Bind(out ReadOnlyObservableCollection<TransactionItem> collection)
+            .Subscribe();
+
+        Items = collection;
+    }
+
+    public ReadOnlyObservableCollection<TransactionItem> Items { get; }
+}
+
+public abstract class TransactionItem
+{
+    public ReadOnlyObservableCollection<TransactionItem> Children { get; protected set; }
+    public string Name { get; set; }
+}
+
+public class TransactionGroup : TransactionItem
+{
+    public TransactionGroup(IGroup<TransactionModel, int, int> group)
+    {
+        group.Cache.Connect()
+            .Transform(x => (TransactionItem)new SingleTransactionItem(x))
+            .Bind(out var children)
+            .Subscribe();
+        Children = children;
+        Name = group.Key.ToString();
+    }
+}
+
+public class SingleTransactionItem : TransactionItem
+{
+    public SingleTransactionItem(TransactionModel transactionModel) : base()
+    {
+        Name = transactionModel.Name;
+    }
+}
+
+public class TransactionModel : ViewModelBase
+{
+    public TransactionModel(string name, int id, int? parentId)
+    {
+        Name = name;
+        Id = id;
+        ParentId = parentId;
+    }
+    
+    public string Name { get; }
+    public int Id { get; }
+    public int? ParentId { get; set; }
+    public int GroupId => ParentId ?? -Id;
 }
--- HistoryPoC/Views/MainView.axaml.cs
+++ HistoryPoC/Views/MainView.axaml.cs
@@ -1,45 +1,40 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
 using Avalonia.Controls;
 using Avalonia.Controls.Models.TreeDataGrid;
+using HistoryPoC.ViewModels;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace HistoryPoC.Views;
 
 public partial class MainView : UserControl
 {
-    private ObservableCollection<Model> models = new ObservableCollection<Model>()
-    {
-        new Model()
-        {
-            Name = "Hola",
-            Children = new ObservableCollection<Model>()
-            {
-                new Model()
-                {
-                    Name="Adiós",
-                    Children = new ObservableCollection<Model>()
-                }
-            }
-        }
-    };
-
     public MainView()
     {
-        Source = new HierarchicalTreeDataGridSource<Model>(models)
+        InitializeComponent();
+        
+        this.WhenAnyValue(x => x.DataContext)
+            .WhereNotNull()
+            .Select(x => CreateSource(((MainViewModel)x).Items))
+            .Subscribe(x =>
+            {
+                TreeDataGrid.Source = x;
+            });
+    }
+
+    [Reactive]
+    public HierarchicalTreeDataGridSource<TransactionItem> Source { get; private set; }
+
+    private HierarchicalTreeDataGridSource<TransactionItem> CreateSource(ReadOnlyObservableCollection<TransactionItem> groups)
+    {
+        return new HierarchicalTreeDataGridSource<TransactionItem>(groups)
         {
             Columns =
             {
-                new HierarchicalExpanderColumn<Model>(new TextColumn<Model, string>("Name", model => model.Name), x => x.Children)
+                new HierarchicalExpanderColumn<TransactionItem>(new TextColumn<TransactionItem,string>("Name", x => x.Name), x => x.Children)
             }
         };
-        
-        InitializeComponent();
     }
-
-    public HierarchicalTreeDataGridSource<Model> Source { get; set; }
-}
-
-public class Model
-{
-    public ObservableCollection<Model> Children { get; set; } = new ObservableCollection<Model>();
-    public string? Name { get; set; }
 }
