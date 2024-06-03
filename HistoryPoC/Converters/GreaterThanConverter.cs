3cecb4cb1bc2b53ccc9439c64830a6d2378d1232
José Manuel Nieto <superjmn@outlook.com>
2024-06-03 13:36:11 +0200

    Improve DataGrid

---
+++ HistoryPoC/Converters/GreaterThanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace HistoryPoC.Converters;
+
+public class GreaterThanConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value == null || parameter == null)
+            return false;
+
+        if (double.TryParse(value.ToString(), out double valueToCompare) &&
+            double.TryParse(parameter.ToString(), out double referenceValue))
+        {
+            return valueToCompare > referenceValue;
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
